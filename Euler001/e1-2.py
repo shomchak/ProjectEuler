bd357704aac7306fe261c3df7d5c07c56465f8a7
shomchak <shomik.chakravarty@gmail.com>
2012-09-11 00:04:25 -0700

    an analytical approach

---
+++ Euler001/e1-2.py
@@ -0,0 +1,14 @@
+# an analytical approach
+
+
+num3s = 1000/3
+num5s = 999/5
+num15s= 1000/15
+
+sum3 = 3*num3s*(num3s+1)/2
+sum5 = 5*num5s*(num5s+1)/2
+sum15 = 15*num15s*(num15s+1)/2
+
+total = sum3+sum5-sum15
+
+print total
