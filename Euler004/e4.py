6de8e52f56583385ed9297fe7f9d0e766c40f56a
shomchak <shomik.chakravarty@gmail.com>
2012-09-12 15:42:16 -0700

    actually removed isPalindrome

--- Euler004/e4.py
+++ Euler004/e4.py
@@ -1,17 +1,12 @@
 # brute force: calculate each of the 900-choose-2 products, testing each for palindrome-ness 
 # as it is calcualted, and keep the largest
-
-def isPalindrome(n):
-    if str(n) == str(n)[::-1]: return True
-    else: return False
     
-def main():
-    max = 0
-    for m in range(100, 1000):
-        for n in range(m, 1000):
-            if str(n) == str(n)[::-1]: 
-                if m*n > max:
-                    max = m*n
-    print max
+max = 0
+for m in range(100, 1000):
+    for n in range(m, 1000):
+        product = m*n
+        if str(product) == str(product)[::-1]: 
+            if product > max:
+                max = product 
+print max
             
-main()
