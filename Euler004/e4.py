00bb609ff02e26a8d871080eb53dd125f5b59c28
shomchak <shomik.chakravarty@gmail.com>
2012-09-12 15:31:16 -0700

    removed sort

--- Euler004/e4.py
+++ Euler004/e4.py
@@ -1,21 +1,17 @@
-# Generate a list of products, sort them in descending order, iterate and test for palindrome-ness
+# brute force: calculate each of the 900-choose-2 products, testing each for palindrome-ness 
+# as it is calcualted, and keep the largest
 
 def isPalindrome(n):
     if str(n) == str(n)[::-1]: return True
     else: return False
     
-def genList():
-    products = []
-    for n in range(100, 1000):
-        for m in range(n, 1000):
-            products.append(n*m)
-    products.sort(reverse=True)
-    return products
-
-def testList():
-    for element in genList():
-        if isPalindrome(element):
-            print element
-            break
-
-testList()
+def main():
+    max = 0
+    for m in range(100, 1000):
+        for n in range(m, 1000):
+            if isPalindrome(m*n):
+                if m*n > max:
+                    max = m*n
+    print max
+            
+main()
