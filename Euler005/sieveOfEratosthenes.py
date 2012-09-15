0355426541fe246928e0650acd3da43b0dc4f17f
shomchak <shomik.chakravarty@gmail.com>
2012-09-14 18:29:32 -0700

    updated prime sieve

--- Euler005/sieveOfEratosthenes.py
+++ Euler005/sieveOfEratosthenes.py
@@ -1,22 +1,13 @@
-def getPrimes(limit):
-	allPrimes = []
-	allNums = {}
-	allNums[1] = False
-	for i in range(2,limit+1):
-		allNums[i] = True
-	currentPrime = 2
-	condition = True
-	while(condition):
-		currentMax = int(limit/currentPrime)
-		for i in range(2,currentMax+1):
-			allNums[currentPrime*i] = False
-		for i in range(currentPrime+1,limit+1):
-			if(allNums[i] == True):
-				currentPrime = i
-				break
-			elif(i == len(allNums)):
-				condition = False
-	for i in range(1, len(allNums)+1):
-		if(allNums[i] == True):
-			allPrimes.append(i)
-	return allPrimes
+def getPrimes(N):
+    composites = {}
+    guess = 2
+    while guess <= N:
+        if guess in composites:
+            for prime in composites[guess]:
+                composites.setdefault(guess+prime,[]).append(prime)
+            del composites[guess]
+        else:
+            yield guess
+            #composites.setdefault(2*guess,[]).append(guess)
+            composites[guess*guess] = [guess]
+        guess += 1
