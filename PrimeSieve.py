52a1f8b688109ff5a7f87426507758068c900354
shomchak <shomik.chakravarty@gmail.com>
2012-09-14 18:25:24 -0700

    fast sieve of eratosthenes

---
+++ PrimeSieve.py
@@ -0,0 +1,13 @@
+def primeGenerator(N):
+    composites = {}
+    guess = 2
+    while guess <= N:
+        if guess in composites:
+            for prime in composites[guess]:
+                composites.setdefault(guess+prime,[]).append(prime)
+            del composites[guess]
+        else:
+            yield guess
+            #composites.setdefault(2*guess,[]).append(guess)
+            composites[guess*guess] = [guess]
+        guess += 1
