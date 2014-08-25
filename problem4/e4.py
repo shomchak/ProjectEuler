8c438930883f60ab766e96ac5151597155ffc9d6
shomchak <shomik.chakravarty@gmail.com>
2014-08-24 19:48:59 -0700

    Problem4: python style

--- problem4/e4.py
+++ problem4/e4.py
@@ -1,12 +1,14 @@
-# brute force: calculate each of the 900-choose-2 products, testing each for palindrome-ness 
-# as it is calcualted, and keep the largest
-    
-max = 0
-for m in range(100, 1000):
-    for n in range(m, 1000):
-        product = m*n
-        if str(product) == str(product)[::-1]: 
-            if product > max:
-                max = product 
-print max
-            
+
+def solve():
+    '''
+    brute force: calculate each of the 900-choose-2 products, testing each for
+    palindrome-ness as it is calcualted, and keep the largest
+    '''
+    maximum = 0
+    for m in range(100, 1000):
+        for n in range(m, 1000):
+            product = m * n
+            if str(product) == str(product)[::-1]:
+                if product > maximum:
+                    maximum = product
+    return maximum
