fa63f7695dbdb8d9db558b131dad7722d48804e5
shomchak <shomik.chakravarty@gmail.com>
2014-08-24 18:44:05 -0700

    Problem3: haskell comments

--- problem3/e3.hs
+++ problem3/e3.hs
@@ -2,11 +2,11 @@ module Euler3 where
 
 solve :: Integer
 solve = solveIter the_number factors
-    where the_number  = 600851475143
-          lt_root p   = fromIntegral p <= (sqrt $ fromIntegral the_number)
-          to_consider = takeWhile lt_root primes
-          factors     = filter (divides the_number) to_consider
+    where the_number = 600851475143
+          lt_root p  = fromIntegral p <= (sqrt $ fromIntegral the_number)
+          factors    = filter (divides the_number) $ takeWhile lt_root primes
 
+-- | Divide <n> by its prime factors until it is 1.
 solveIter :: Integer -> [Integer] -> Integer
 solveIter n [] = n
 solveIter n (f:fs)
@@ -16,6 +16,7 @@ solveIter n (f:fs)
         where m = mod n f
               q = quot n f
 
+-- | A list of all primes.
 primes :: [Integer]
 primes = 2 : filter prime [3,5..]
     where prime n     = all (not . divides n) $ takeWhile (lt_half n) primes
