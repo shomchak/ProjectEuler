66be4fd7784caa16c090f02a5418bdd24f7dec6c
shomchak <shomik.chakravarty@gmail.com>
2014-08-24 18:39:29 -0700

    Problem3: python style

--- problem3/e3.py
+++ problem3/e3.py
@@ -1,27 +1,26 @@
-# Starting with 2, find new primes and divide into the number.
 
-def divideOut(N,p):
-    if N%p == 0:
-        return divideOut(N/p,p)
-    else:
-        return N
+def solve(number=600851475143):
+    '''
+    Starting with 2, find new primes and divide the number until it is 1.
+    '''
+    primes = set((2,))
+    prime_candidate = 3
+    current = divide_out(number, 2)
 
-def main():
-    primes = set([2])
-    prime = 3
-    number = 600851475143
-    number = divideOut(number,2)
-    while(number != 1):
-        isPrime = True
+    while current != 1:
+        is_prime = True
         for p in primes:
-            if prime%p == 0:
-                isPrime = False
-                prime += 2
+            if prime_candidate % p == 0:
+                is_prime = False
+                prime_candidate += 2
                 break
-        if isPrime:
-            number = divideOut(number,prime)
-            primes.add(prime)
-            prime += 2
-    print prime-2
+        if is_prime:
+            current = divide_out(current, prime_candidate)
+            primes.add(prime_candidate)
+            prime_candidate += 2
 
-main()
+    return prime_candidate - 2
+
+
+def divide_out(n, p):
+    return divide_out(n / p, p) if n % p == 0 else n
