b857731aa66e1e879b6d969f83e464bd8e1bd3dd
shomchak <shomik.chakravarty@gmail.com>
2014-08-29 14:52:30 -0700

    Problem5: python style

--- problem5/e5.py
+++ problem5/e5.py
@@ -1,27 +1,32 @@
-# A straight forward prime factorization approach. First find the prime 
-# factorizations of each number 1-20. Then take the union of all the 
-# factorizations and then multiply out the union to get the number.
+'''
+A straight forward prime factorization approach. First find the prime
+factorizations of each number 1-20. Then take the union of all the
+factorizations and then multiply out the union to get the number.
+'''
+
 
 import math
 import sieveOfEratosthenes
 
-def divideOut(N,p,times):
-    if N%p == 0:
+
+def divide_out(n, p, times):
+    if n % p == 0:
         times += 1
-        return divideOut(N/p,p,times)
+        return divide_out(n / p, p, times)
     else:
         return times
 
-def factors(n,primes):
-    facts = {}
+
+def factors(n, primes):
+    factors = {}
     for p in primes:
-        facts[p] = divideOut(n,p,0)
-    return facts
-    
-def main():
-    num = 20
+        factors[p] = divide_out(n, p, 0)
+    return factors
+
+
+def solve(num):
     primes = list(sieveOfEratosthenes.getPrimes(num))
-    primefactors = map(factors,range(2,num+1),[primes]*(num-1))
+    primefactors = map(factors, range(2, num + 1), [primes] * (num - 1))
     minfactors = {}
     for p in primes:
         minfactors[p] = 0
@@ -31,7 +36,11 @@ def main():
                 minfactors[key] = factor[key]
     product = 1
     for prime in minfactors.keys():
-        product *= math.pow(prime,minfactors[prime])
-    print int(product)
+        product *= math.pow(prime, minfactors[prime])
+    return product
 
-main()
+
+if __name__ == '__main__':
+    num = 20
+    product = solve(num)
+    print int(product)
