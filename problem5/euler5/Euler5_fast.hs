eba10d0690d4662126030f2fc3208ff48de63b01
shomchak <shomik.chakravarty@gmail.com>
2014-10-15 02:42:30 -0700

    add product-accumulating haskell solution

--- problem5/euler5/Euler5_fast.hs
+++ problem5/euler5/Euler5_fast.hs
@@ -7,9 +7,17 @@ import Primes (primes)
 
 main :: IO ()
 main = do args <- getArgs
-          putStrLn $ show $ solve $ read $ args !! 0
+          putStrLn $ show $ solve' $ read $ args !! 0
 
+-- | Accumulate a list of primes and reduce them at the end.
 solve :: Integer -> Integer
 solve n = foldl' (*) 1 $ takeWhile (<=n) primes >>= primeContent n
             where primeContent n p | p > n     = []
                                    | otherwise = p : (primeContent (quot n p) p)
+
+-- | Accumulate a product of primes as you calculate them.
+solve' :: Integer -> Integer
+solve' n = foldl' (\ acc p -> acc * p ^ (primeContent n p)) 1 $ takeWhile (<=n) primes
+            where primeContent n p | p > n     = 0
+                                   | otherwise = 1 + (primeContent (quot n p) p)
+
