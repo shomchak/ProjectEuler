a2b832a14f23077e627c377f5921178bb5b2ea61
Shomik Chakravarty <shomik.chakravarty@planet-labs.com>
2014-10-20 21:31:47 -0700

    cleanup primes

--- problem5/euler5/Primes.hs
+++ problem5/euler5/Primes.hs
@@ -1,18 +1,11 @@
-module Primes (
-    primes
-    , lt_sqrt
-    , divides) where
+module Primes ( primes
+              , lt_sqrt
+              , divides
+              ) where
 
 -- | A list of all primes.
 primes :: [Integer]
 primes = 2 : filter isPrime [3,5..]
-    where isPrime n = all (not . divides n) $ takeWhile (lt_sqrt n) primes
-
--- | Whether the square root of a number is greater than another number.
-lt_sqrt :: (Integral a) => a -> a -> Bool
-lt_sqrt n p = p <= (floor $ sqrt $ fromIntegral n)
-
--- | Whether a number is evenly divided by another number.
-divides :: (Integral a) => a -> a -> Bool
-divides n p = mod n p == 0
-
+  where isPrime n   = all (not . divides n) $ takeWhile (lt_sqrt n) primes
+        divides n p = mod n p == 0
+        lt_sqrt n p = p <= (floor $ sqrt $ fromIntegral n)
