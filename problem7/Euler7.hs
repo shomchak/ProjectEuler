fbe7662e178ebe95e69a89d58bbfa2aa709799ce
Shomik Chakravarty <shomik.chakravarty@planet-labs.com>
2014-10-20 21:36:01 -0700

    added haskell solutiom for problem 7

---
+++ problem7/Euler7.hs
@@ -0,0 +1,10 @@
+module Main where
+
+main = print $ show $ primes !! n where n = 10001
+
+-- | A list of all primes.
+primes :: [Integer]
+primes = 2 : filter isPrime [3,5..]
+  where isPrime n   = all (not . divides n) $ takeWhile (lt_sqrt n) primes
+        divides n p = mod n p == 0
+        lt_sqrt n p = p <= (floor $ sqrt $ fromIntegral n)
